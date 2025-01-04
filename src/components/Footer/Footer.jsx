236d65d75d6c79252446b73f5059453937afcbfe
Sushanta65 <sushanta9119@gmail.com>
2025-01-04 21:31:41 +0600

    Footer added

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -5,10 +5,12 @@ const Footer = () => {
     <footer className="bg-red-600  py-6">
       <div className="w-4/5 mx-auto text-center text-white">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Susanto Chandra Das. All rights reserved.
+          &copy; {new Date().getFullYear()} Susanto Chandra Das. All rights
+          reserved.
         </p>
         <p className="text-sm">
-          Made with <span className="text-red-500">❤</span> using React and Tailwind CSS.
+          Made with <span className="text-red-500">❤</span> using React and
+          Tailwind CSS.
         </p>
       </div>
     </footer>
