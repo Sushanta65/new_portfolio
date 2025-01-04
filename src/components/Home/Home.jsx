e6272aca45eb4fe0e6b76612989fdcf2cf15c253
Sushanta65 <sushanta9119@gmail.com>
2025-01-04 21:32:47 +0600

    Resolved Some Problem of Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,24 @@
-import React from 'react';
-import HeroSection from '../HereSection/HeroSection';
-import AboutMe from '../AboutMe.jsx/AboutMe';
-import SkillsSection from '../SkillsSection/SkillsSection';
-import Education from '../Educations/Education';
-import ProjectsSection from '../Projects/Projects';
-import Contact from '../Contact/Contact';
-import Footer from '../Footer/Footer';
+import React from "react";
+import HeroSection from "../HereSection/HeroSection";
+import AboutMe from "../AboutMe.jsx/AboutMe";
+import SkillsSection from "../SkillsSection/SkillsSection";
+import Education from "../Educations/Education";
+import ProjectsSection from "../Projects/Projects";
+import Contact from "../Contact/Contact";
+import Footer from "../Footer/Footer";
 
 const Home = () => {
-    return (
-        <div id="home">
-            <HeroSection></HeroSection>
-            <AboutMe></AboutMe>
-            <SkillsSection></SkillsSection>
-            <Education></Education>
-            <ProjectsSection></ProjectsSection>
-            <Contact></Contact>
-            <Footer></Footer>
-        </div>
-    );
+  return (
+    <div id="home">
+      <HeroSection></HeroSection>
+      <AboutMe></AboutMe>
+      <SkillsSection></SkillsSection>
+      <Education></Education>
+      <ProjectsSection></ProjectsSection>
+      <Contact></Contact>
+      <Footer></Footer>
+    </div>
+  );
 };
 
 export default Home;
